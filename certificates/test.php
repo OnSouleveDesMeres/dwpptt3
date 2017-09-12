91327dcc363bfee13880b5904c534da111d8804e
Sylvain COMBRAQUE <sylvaincombraque@hotmail.fr>
2017-09-12 09:50:59 +0200

    Bugfix image generating print

--- certificates/test.php
+++ certificates/test.php
@@ -1,9 +1,41 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: root
- * Date: 21/08/17
- * Time: 21:40
+ * User: sylvain
+ * Date: 10/09/17
+ * Time: 22:49
  */
 
-echo date('Y-m-d', strtotime("+30 days"));
+// Définition du content-type
+    header('Content-Type: image/png');
+
+// Création de l'image
+    $im = imagecreatetruecolor(180, 67);
+
+// Création de quelques couleurs
+    $white = imagecolorallocate($im, 255, 255, 255);
+    $grey = imagecolorallocate($im, 128, 128, 128);
+    $black = imagecolorallocate($im, 0, 0, 0);
+    imagefilledrectangle($im, 0, 0, 399, 100, $white);
+
+// Le texte à dessiner
+    $text = 'Dr. DUCHAUSSOY B.';
+    $text1 = 'Médecin remplaçante';
+    $text2 = $_GET['medNum'] . ' ' . $_GET['medStreet'];
+    $text3 = $_GET['medCP'] . ' ' . $_GET['medCity'];
+    $text4 = 'N° Ordre : 015 215 632';
+// Remplacez le chemin par votre propre chemin de police
+    $font = '/usr/share/fonts/cantarell/Cantarell-Regular.otf';
+    $save = "../img/" . $ci . ".png";
+
+// Ajout du texte
+    imagettftext($im, 12, 0, 5, 13, $black, $font, $text);
+    imagettftext($im, 12, 0, 5, 26, $black, $font, $text1);
+    imagettftext($im, 12, 0, 5, 39, $black, $font, $text2);
+    imagettftext($im, 12, 0, 5, 52, $black, $font, $text3);
+    imagettftext($im, 12, 0, 5, 65, $black, $font, $text4);
+
+// Utiliser imagepng() donnera un texte plus claire,
+// comparé à l'utilisation de la fonction imagejpeg()
+imagepng($im, $save);
+imagepng($im);
