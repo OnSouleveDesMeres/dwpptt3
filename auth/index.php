cf7bdc522e23c96a7ddb587bfcb1a6fc9ecd0069
Sylvain COMBRAQUE <sylvaincombraque@hotmail.fr>
2017-09-19 18:12:07 +0200

    add sessions

--- auth/index.php
+++ auth/index.php
@@ -10,6 +10,10 @@ require_once '../database/Users.class.php';
 require_once '../database/Tokens.class.php';
 require_once '../database/key.function.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if(isset($_POST['email']) && $_POST['email'] != null && isset($_POST['password']) && $_POST['password'] != null){
 
     $user = Users::createFromMailAndPassword($_POST['email'],$_POST['password']);
@@ -18,10 +22,11 @@ if(isset($_POST['email']) && $_POST['email'] != null && isset($_POST['password']
 
         $chaine = hash_hmac('sha256', $user[0]->getId().$user[0]->getMail().getKey(), getKey());
         Tokens::insert($user[0]->getId(), $chaine);
-        setcookie('tt3',$chaine);
+        setcookie("tt3","".$chaine, 0, "/");
 
     }
 
 }
 
-header('Location: http://tt3y5d3gkz2x5l4d.onion/');
+//header('Location: http://tt3y5d3gkz2x5l4d.onion/');
+header('Location: http://tt3y5d3gkz2x5l4d.onion/dwpptt3/');
